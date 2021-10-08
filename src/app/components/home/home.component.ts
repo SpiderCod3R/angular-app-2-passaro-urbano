cc909bc16b08b84f9b49de82d5ded41aca5da828
THUNDER MASTER <thndrmaster@gmail.com>
2021-10-08 14:35:52 -0300

    141. App2 - Injetando o serviço de ofertas no componente Home parte 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'src/app/ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    console.log(this.ofertasService.get_ofertas());
+    
   }
 
 }
