525fa749513d2b7bfe22c3ea991250c4fe3bf5e3
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-10-21 10:56:28 -0300

    164, 165  Ajustando o template do DiversaoComponent

--- src/app/components/entertainment/entertainment.component.ts
+++ src/app/components/entertainment/entertainment.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'src/app/ofertas.service';
+import { Oferta } from 'src/app/shared/oferta.model';
 
 @Component({
   selector: 'app-entertainment',
   templateUrl: './entertainment.component.html',
-  styleUrls: ['./entertainment.component.scss']
+  styleUrls: ['./entertainment.component.scss'],
+  providers: [ OfertasService ]
 })
 export class EntertainmentComponent implements OnInit {
+  public ofertas!: Oferta[]
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertaService.get_ofertas_por_categoria('diversao').
+      then( (ofertas: Oferta[]) => this.ofertas = ofertas)
   }
 
 }
