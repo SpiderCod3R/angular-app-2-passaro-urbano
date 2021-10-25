ef45aa53561c3ad5b30d3c29bb1b6987e76001b8
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-10-25 09:39:30 -0300

    179. Recuperando parâmetros da rota parent (pai) nas rotas filhas

--- src/app/components/offer/how-to/how-to.component.ts
+++ src/app/components/offer/how-to/how-to.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-how-to',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowToComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log("ID DA ROTA PAI: ", this.route.parent?.snapshot.params['id'])
   }
 
 }
--- src/app/components/offer/where-stay/where-stay.component.ts
+++ src/app/components/offer/where-stay/where-stay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-where-stay',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhereStayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log("ID DA ROTA PAI: ", this.route.parent?.snapshot.params['id'])
   }
 
 }
