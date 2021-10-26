179e3ca648cb8b6f5af2a2a95ce89bb9c467326e
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-10-26 14:18:16 -0300

    189. Observables na prática parte 5 - Memory leak e Unsubscribe

--- src/app/components/offer/offer.component.ts
+++ src/app/components/offer/offer.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from 'src/app/shared/oferta.model';
 import { OfertasService } from '../../ofertas.service'
 
-import { Observable, interval, Observer } from 'rxjs';
+import { Observable, interval, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss']
 })
-export class OfferComponent implements OnInit {
+export class OfferComponent implements OnInit, OnDestroy {
   public oferta!: Oferta
+
+  private tempoObservableSubscription!: Subscription
+  private meuObservableTesteSubscription!: Subscription
 
   constructor(
     private route: ActivatedRoute, 
@@ -23,17 +26,30 @@ export class OfferComponent implements OnInit {
         this.oferta = oferta
       })
     
-    // let tempo = interval(500)
-    // tempo.subscribe( ( intervalo: number ) => {
-    //   console.log(intervalo)
-    // })
+    let tempo = interval(2000)
+    this.tempoObservableSubscription = tempo.subscribe( ( intervalo: number ) => {
+      console.log(intervalo)
+    })
 
     // Observable (Observavel)
-    let meuObservableTeste = new Observable(( observer: Observer<String> ) => {
-      observer.next('primeiro evendo da stream')
+    let meuObservableTeste = new Observable(( observer: Observer<number> ) => {
+      observer.next(1)
+      observer.next(7)
+      observer.complete()
+      observer.next(11)
+      observer.error('Algum erro foi encontrado na stream de eventos')
     })
 
     // Observable (Observador)
-    meuObservableTeste.subscribe( ( resultado: any ) => console.log(resultado) )
+    this.meuObservableTesteSubscription = meuObservableTeste.subscribe( 
+      ( resultado: number ) => console.log(resultado +10),
+      ( error: string ) => console.log(error),
+      () => console.log("Stream de eventos foi finalizada")
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.meuObservableTesteSubscription.unsubscribe()
+    this.tempoObservableSubscription.unsubscribe()
   }
 }
