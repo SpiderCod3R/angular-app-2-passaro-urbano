3d3a2e4b732eee2f4e059cf7c88852ac1669290b
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-10-27 15:41:57 -0300

    199. Lógica da pesquisa de ofertas parte 4 - Subject e switchMap

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { OfertasService } from 'src/app/ofertas.service';
 import { Oferta } from 'src/app/shared/oferta.model';
+
+// import 'rxjs/add/operator/switchMap' FUNCIONA NO ANGULAR 4
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,18 +14,32 @@ import { Oferta } from 'src/app/shared/oferta.model';
 export class HeaderComponent implements OnInit {
 
   public ofertas!: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject()
 
   constructor(private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa.
+      pipe(
+        switchMap((termo: string) => {
+          return this.ofertaService.pesquisa_ofertas(termo)
+        })
+      )
+      this.ofertas.subscribe( (ofertas: Oferta[]) => {
+        console.log(ofertas)
+      })
   }
 
-  public pesquisa(termoDaPesquisa: String): void {
+  public pesquisa(termoDaPesquisa: string): void {
     // console.log((<HTMLInputElement>evento.target).value)
-    this.ofertas = this.ofertaService.pesquisa_ofertas(termoDaPesquisa)
-    this.ofertas.subscribe( 
-      (oferta: Oferta[]) => console.log(oferta),
-      (error: any) => console.log('Erro status: ', error.status))
+    // this.ofertas = this.ofertaService.pesquisa_ofertas(termoDaPesquisa)
+    // this.ofertas.subscribe( 
+    //   (oferta: Oferta[]) => console.log(oferta),
+    //   (error: any) => console.log('Erro status: ', error.status),
+    //   () => console.log("Fluxo de Eventos completo!")
+    // )
+    this.subjectPesquisa.next(termoDaPesquisa)
+
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import {} from '@angular/common/http'
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
@@ -10,7 +11,7 @@ import { API_URL } from './app.api';
 @Injectable({ providedIn: 'root' })
 export class OfertasService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient ) { }
 
   // Efetuar um requisicao HTTP
   // Retornar um Promisse de Ofertas
@@ -18,7 +19,7 @@ export class OfertasService {
     return this.httpClient.
       get(`${API_URL}/ofertas?destaque=true`).
       toPromise().
-      then((resposta: any) => resposta )
+      then( (resposta: any) => resposta )
   }
 
   public get_ofertas_por_categoria(categoria: String): Promise<Oferta[]>{
@@ -57,8 +58,26 @@ export class OfertasService {
     return this.httpClient.
       get(`${API_URL}/ofertas?descricao_oferta_like=${termo}`).
       pipe( 
-        retry(10),
+        retry(10), 
         map( (resposta: any) => resposta) 
       )
   }
+
+  // Nas versoes Anteriores como a Angular 4 O uso de 
+  // Response era e ainda é utilizado
+  // import { Http, Response } from '@angular/http'
+  // import { Observable } from 'rxjs/Observable';
+  //
+  // import 'rxjs/add/operator/toPromisse';
+  // import 'rxjs/add/operator/map';
+  // import 'rxjs/add/operator/retry';
+  //
+  // constructor(private http: Http) { }
+  //
+  // public pesquisa_ofertas(termo: String): Observable<Oferta[]> {
+  //   return this.http.
+  //     get(`${API_URL}/ofertas?descricao_oferta_like=${termo}`).
+  //       retry(10). 
+  //       map( (resposta: Response) => resposta) 
+  // }
 }
