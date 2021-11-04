cfe5d93d1808e70a58b26ac3dfe8413b8942d009
ALEXANDRE SAMPAIO <thndrmaster@gmail.com>
2021-11-04 16:44:31 -0300

    223. Two-Way-Binding dos dados do formulário [ Error no number]

--- src/app/components/purchase-order/purchase-order.component.ts
+++ src/app/components/purchase-order/purchase-order.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: number = 1
+  public complemento: string = ''
+  public forma_pagamento: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco
+    console.log(this.endereco)
+  }
+  
+  public atualizaNumero(numero: string): void {
+    this.numero = parseInt(numero)
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    this.complemento = complemento
+  }
+
+  public atualizaFormaPagamento(formaDePagamento: string): void {
+    this.forma_pagamento = formaDePagamento
   }
 
 }
